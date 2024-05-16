afafe3d1a99a141473810158922799412b2d548d
Jaisonaj77 <jaisonaj77@gmail.com>
2024-05-16 14:27:38 +0530

    Update oraclescript.sql

--- oraclescript.sql
+++ oraclescript.sql
@@ -1,3 +1,3 @@
-CREATE TABLE test1 (     name VARCHAR2(50),     address VARCHAR2(100),     age NUMBER,     mobile_no VARCHAR2(15) );
-INSERT INTO test1 (name, address, age, mobile_no) 
+CREATE TABLE mani (     name VARCHAR2(50),     address VARCHAR2(100),     age NUMBER,     mobile_no VARCHAR2(15) );
+INSERT INTO mani (name, address, age, mobile_no) 
 VALUES ('John Doe', '123 Main St, City', 30, '123-456-7890');
