2998e2e9ee04f4da24bb6e4f96ff8f4edcc85b5a
Jaisonaj77 <jaisonaj77@gmail.com>
2024-05-16 17:01:27 +0530

    Update oraclescript.sql

--- oraclescript.sql
+++ oraclescript.sql
@@ -1,3 +1,6 @@
-CREATE TABLE mani (     name VARCHAR2(50),     address VARCHAR2(100),     age NUMBER,     mobile_no VARCHAR2(15) );
-INSERT INTO mani (name, address, age, mobile_no) 
-VALUES ('John Doe', '123 Main St, City', 30, '123-456-7890');
+CREATE OR REPLACE PROCEDURE sample_procedure IS
+BEGIN
+  -- Your procedure logic here
+  DBMS_OUTPUT.PUT_LINE('Sample procedure executed successfully.');
+END;
+/
