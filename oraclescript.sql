cc9851e23b7b8adcc03685c40896a7b4331fd0e0
Jaisonaj77 <jaisonaj77@gmail.com>
2024-05-16 12:21:03 +0530

    Create oraclescript.sql

---
+++ oraclescript.sql
@@ -0,0 +1,3 @@
+CREATE TABLE test1 (     name VARCHAR2(50),     address VARCHAR2(100),     age NUMBER,     mobile_no VARCHAR2(15) );
+INSERT INTO jasion (name, address, age, mobile_no) 
+VALUES ('John Doe', '123 Main St, City', 30, '123-456-7890');
